12d5f348b842939106db96e276858048413d52e3
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-11-25 22:34:08 +0300

    Global error handling and loading effect added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'image.tmdb.org',
+          port: '', // Leave empty unless the domain uses a specific port
+          pathname: '/t/p/original/**', // Allow specific path patterns
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
