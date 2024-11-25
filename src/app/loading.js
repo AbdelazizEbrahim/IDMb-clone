0d95cb747c8e8821e6e0f2c4970a32fc2840f1b4
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-11-25 22:42:12 +0300

    Single movie page added

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,8 +1,15 @@
-import Image from "next/image";
-export default function loading() {
-    return (
-      <div className="flex justify-center">
-        <Image className="h-96" src="spinner.svg" alt="loading..." />
-      </div>
-    );
-  }
+import Image from 'next/image';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <Image 
+        className="h-96" 
+        src="/spinner.svg" 
+        alt="loading..." 
+        width={384} // specify width based on the actual size of the image
+        height={384} // specify height based on the actual size of the image
+      />
+    </div>
+  );
+}
